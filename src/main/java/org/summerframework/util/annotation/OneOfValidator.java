684f09e9e7071aa2305c2fe60856c6717f64c6e2
Summer Boot Framework <ouleji@yahoo.com>
2022-04-04 12:17:17 -0400

    replace javax.validation with jakarta.validation

--- src/main/java/org/summerframework/util/annotation/OneOfValidator.java
+++ src/main/java/org/summerframework/util/annotation/OneOfValidator.java
@@ -3,8 +3,8 @@ package org.summerframework.util.annotation;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 import com.google.common.base.Preconditions;
 
