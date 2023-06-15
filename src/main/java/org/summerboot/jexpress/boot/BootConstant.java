6edbb04fb2e849cd78c01dae62b4eed34e893892
Summer Boot Framework <ouleji@yahoo.com>
2023-06-15 14:25:59 -0400

    refactoring

--- src/main/java/org/summerboot/jexpress/boot/BootConstant.java
+++ src/main/java/org/summerboot/jexpress/boot/BootConstant.java
@@ -24,7 +24,7 @@ import org.summerboot.jexpress.util.ApplicationUtil;
 public interface BootConstant {
 
     //version
-    String VERSION = "SummerBoot.jExpress 2.3.6";
+    String VERSION = "SummerBoot.jExpress 2.3.7";
 
     //logging metadata
     String LOG4J2_KEY = "log4j.configurationFile";
