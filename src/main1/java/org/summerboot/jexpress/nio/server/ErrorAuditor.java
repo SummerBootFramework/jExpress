12e8624b7a254834ed940936416b7b52287b49e6
Summer Boot Framework <ouleji@yahoo.com>
2023-10-16 16:28:32 -0400

    refactoring

--- src/main1/java/org/summerboot/jexpress/nio/server/ErrorAuditor.java
+++
@@ -1,9 +0,0 @@
-package org.summerboot.jexpress.nio.server;
-
-/**
- *
- * @author Changski Tie Zheng Zhang 张铁铮, 魏泽北, 杜旺财, 杜富贵
- */
-public interface ErrorAuditor {
-    String beforeSendingError(String errorContent);
-}
