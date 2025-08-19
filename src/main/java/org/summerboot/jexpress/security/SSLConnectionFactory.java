2893160ba13d6ce6dded18c4789f28b857de6bd0
Summer Boot Framework <admin@dulawoffice.com>
2025-08-19 10:40:14 -0400

    refactoring

--- src/main/java/org/summerboot/jexpress/security/SSLConnectionFactory.java
+++ src/main/java/org/summerboot/jexpress/security/SSLConnectionFactory.java
@@ -19,6 +19,8 @@ public class SSLConnectionFactory extends SSLSocketFactory {
     protected final SSLSocketFactory sf;
 
     /**
+     * Creates an SSLConnectionFactory with the specified KeyManagerFactory and TrustManagerFactory.
+     *
      * @param kmf
      * @param tmf
      * @param protocol default TLSv1.3 if null
@@ -29,11 +31,25 @@ public class SSLConnectionFactory extends SSLSocketFactory {
                 protocol);
     }
 
+    /**
+     * Creates an SSLConnectionFactory with the specified KeyManagers and TrustManagers.
+     *
+     * @param kms
+     * @param tms
+     * @param protocol
+     * @throws GeneralSecurityException
+     * @throws IOException
+     */
     public SSLConnectionFactory(KeyManager[] kms, TrustManager[] tms, String protocol) throws GeneralSecurityException, IOException {
         SSLContext sslCtx = SSLUtil.buildSSLContext(kms, tms, protocol == null ? "TLSv1.3" : protocol);
         sf = sslCtx.getSocketFactory();
     }
 
+    /**
+     * Creates an SSLConnectionFactory with the specified SSLSocketFactory.
+     *
+     * @param sf
+     */
     public SSLConnectionFactory(SSLSocketFactory sf) {
         this.sf = sf;
     }
